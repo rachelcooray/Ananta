6ff8fd82511b035a859433774b4ba3cf7bed2831
rachelcooray <rachelcooray@gmail.com>
2026-02-02 00:07:39 +0530

    Update About page SEO description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ import SEO from '../components/SEO';
 const About = () => {
     return (
         <div className="pt-24 bg-ananta-bg min-h-screen">
-            <SEO title="About Us" description="We are a media and narrative consultancy defining legacy in the digital age." />
+            <SEO title="About Us" description="We are a media and narrative consultancy defining legacy in the digital age" />
             <Mission />
         </div>
     );
